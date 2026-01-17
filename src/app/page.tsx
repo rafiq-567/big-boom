90fbc66b62daff58594119eed8de66e0c7adcd8a
unknown <mdrafiqulakhter0@gmail.com>
2026-01-18 00:13:09 +0600

    some fix3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+
+
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-gray-50 py-10">
       
       Hello World!
-      <Button>Click Me</Button>
+      
       
      
     </main>
